ccab1f879bc556f7ba78119ee4e0bbe6adfa53ef
krzychu <krzysztofnap@yahoo.com>
2024-05-03 13:23:23 +0200

    update1

---
+++ gui.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var labelka : Label = %Label
+@onready var fps : Label = %fps_label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	labelka.text = "Host OS: " + OS.get_name()
+
+func _physics_process(delta : float) -> void:
+	fps.text = "FPS: " + str( Engine.get_frames_per_second() )
